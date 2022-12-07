74993d0c2f467def5f882ba345af90ac17a1bb8f
Vadim <vadim192000@gmail.com>
2022-12-07 16:05:35 +0300

    project complete

--- pages/burgers/index.js
+++ pages/burgers/index.js
@@ -1,7 +1,44 @@
-const Burgers = () => {
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../../styles/Burgers.module.css'
+
+export const getStaticProps = async () => {
+	const res = await fetch('http://localhost:5000/items')
+	const data = await res.json()
+
+	return {
+		props: { burgers: data },
+	}
+}
+
+const Burgers = ({ burgers }) => {
 	return (
 		<div>
 			<h1>Наши бургеры</h1>
+			{burgers.map(burger => {
+				return (
+					<Link
+						href={`/burgers/${burger.id}`}
+						key={burger.id}
+						className={styles.burgerCard}
+					>
+						<div className={styles.imageContainer}>
+							<Image
+								src={`${burger.image}`}
+								alt={`${burger.name}`}
+								width={600}
+								height={600}
+								layout='responsive'
+								objectFit='cover'
+							/>
+						</div>
+						<div>
+							<h3>{burger.name}</h3>
+							<p>{burger.desc}</p>
+						</div>
+					</Link>
+				)
+			})}
 		</div>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,35 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
-			<h1 className={`${styles.title} font-effect-fire-animation`}>Главная</h1>
-			<p className={styles.text}>
-				Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное
-				мясо. О других составляющих начинки можно поспорить, ведь это дело
-				вкуса.
-			</p>
-			<p className={styles.text}>
-				Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество
-				обслуживания, правильная атмосфера заведения.
-			</p>
-			<Link href='/burgers' className={styles.btn}>
-				Все бургеры
-			</Link>
-		</div>
+		<>
+			<Head>
+				<title>Жирные бургеры | Главная</title>
+				<meta name='title' content='Жирные бургеры'></meta>
+			</Head>
+			<div className={styles.container}>
+				<h1 className={`${styles.title} font-effect-fire-animation`}>
+					Главная
+				</h1>
+				<div className={styles.mainImage}>
+					<Image src='/fatburger.png' alt='burger' width={400} height={300} />
+				</div>
+				<p className={styles.text}>
+					Что такое идеальный бургер? Лист свежего салата, мягкие булочки,
+					сочное мясо. О других составляющих начинки можно поспорить, ведь это
+					дело вкуса.
+				</p>
+				<p className={styles.text}>
+					Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество
+					обслуживания, правильная атмосфера заведения.
+				</p>
+				<Link href='/burgers' className={styles.btn}>
+					Все бургеры
+				</Link>
+			</div>
+		</>
 	)
 }
